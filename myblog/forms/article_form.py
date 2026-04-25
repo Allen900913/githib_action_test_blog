d8805f7648a6725114d0848a31edfe579768cf4f
簡劭宸 <181815853+Allen900913@users.noreply.github.com.>
2026-04-26 00:42:15 +0800

    Add GHCR build workflow

---
+++ myblog/forms/article_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired
+
+class ArticleForm(FlaskForm):
+    title = StringField(label="標題：", validators=[DataRequired()])
+    content = TextAreaField(label="內容：", validators=[DataRequired()])
+    submit = SubmitField(label="發佈")
---
+++ myblog/forms/login_form.py
@@ -0,0 +1,8 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField(label="用戶帳號：", validators=[DataRequired()])
+    password = PasswordField(label="密碼：", validators=[DataRequired()])
+    submit = SubmitField(label="登錄")
